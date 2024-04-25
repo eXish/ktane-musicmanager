969a0997852cc751f0c1acebeb35737c696f28a5
Brendan King <brendan@brendanking.com>
2024-04-25 15:13:07 -0400

    Reuploaded

---
+++ Assets/MusicManagerService.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using MusicManagerAssembly;
+using UnityEngine;
+
+[RequireComponent(typeof(KMService))]
+[RequireComponent(typeof(KMGameInfo))]
+[RequireComponent(typeof(KMModSettings))]
+public class MusicManagerService : MonoBehaviour
+{
+	private KMGameInfo _gameInfo;
+	private KMModSettings _modSettings;
+	
+	private void Awake()
+	{
+		_gameInfo = GetComponent<KMGameInfo>();
+		_modSettings = GetComponent<KMModSettings>();
+	}
+
+	private void OnEnable()
+	{
+		KMGameInfo gameInfo = _gameInfo;
+		gameInfo.OnStateChange = (KMGameInfo.KMStateChangeDelegate)Delegate.Combine(gameInfo.OnStateChange, new KMGameInfo.KMStateChangeDelegate(OnStateChange));
+		StartCoroutine(Setup());
+	}
+
+	private void OnDisable()
+	{
+		ModMusicManager.Reset();
+		KMGameInfo gameInfo = _gameInfo;
+		gameInfo.OnStateChange = (KMGameInfo.KMStateChangeDelegate)Delegate.Remove(gameInfo.OnStateChange, new KMGameInfo.KMStateChangeDelegate(OnStateChange));
+	}
+
+	private void OnStateChange(KMGameInfo.State state)
+	{
+		if (state == null)
+		{
+			ModMusicManager.OnEnterGameplay();
+		}
+	}
+
+	private IEnumerator Setup()
+	{
+		yield return null;
+		ModMusicManager.Setup();
+		ModSettingsLoader.AddFromModSettings(_modSettings.Settings);
+		yield break;
+	}
+}
